1020a12f9ddc23e0f5fc28b8b20766626524162c
Lepias <topiasleppakoski@gmail.com>
2022-03-19 00:33:30 +0200

    sama korjaus vielä tähän add tiedostoon

--- add.php
+++ add.php
@@ -1,9 +1,9 @@
 <?php
 header('Access-Control-Allow-Origin: http://localhost:3000');
 header('Access-Control-Allow-Credentials: true');
-header('Access.Control-Allow-Methods: PUT, DELETE, GET, POST, OPTIONS');
-header('Access.Control-Allow-Headers: Content-Type');
-header('Access.Control-Allow-Max-Age: 3600');
+header('Access-Control-Allow-Methods: PUT, DELETE, GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Max-Age: 3600');
 header('Content-Type: application/json');
 
 if($_SERVER['REQUEST_METHOD'] === 'OPTIONS' ){
